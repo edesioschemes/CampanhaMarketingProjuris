87733eb3cfc0b1027cf9c85cc46b48a3a08f01c2
D:\Projetos <edesio.schemes@gmail.com>
2018-05-14 23:31:43 -0300

    Initial commit

---
+++ src/main/java/com/campanhamarketing/CampanhaMarketingApplication.java
@@ -0,0 +1,12 @@
+package com.campanhamarketing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampanhaMarketingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampanhaMarketingApplication.class, args);
+	}
+}
