716cf147c940814b0d912a5b5f5e415cf1bdbf10
D:\Projetos <edesio.schemes@gmail.com>
2018-05-15 00:32:05 -0300

    Inicio

---
+++ src/main/java/com/campanhamarketing/controller/MainController.java
@@ -0,0 +1,27 @@
+package com.campanhamarketing.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController {
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String index() {
+
+		return "index";
+	}
+
+	@RequestMapping(value = "/home", method = RequestMethod.GET)
+	public String home() {
+
+		return "home";
+	}
+
+	@RequestMapping(value = "/acessoNegado", method = RequestMethod.GET)
+	public String acessoNegado() {
+
+		return "acessoNegado";
+	}
+}
